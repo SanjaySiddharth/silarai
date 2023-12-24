c4ab78c38f319bc20753423fec0e781fc6909324
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2023-12-24 21:29:45 +0530

    Initial Commit

---
+++ silarai/silaraiApp.swift
@@ -0,0 +1,17 @@
+//
+//  silaraiApp.swift
+//  silarai
+//
+//  Created by Sanjay Siddharth on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct silaraiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
